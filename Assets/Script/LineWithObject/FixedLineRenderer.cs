29546d84ccafce9e362322f67445755e2c6c5c36
ootsuki <ootsuki@koo-ki.co.jp>
2014-10-20 23:27:04 +0900

    Cube間でLineRendererを出せるようにする（一旦終了）

---
+++ Assets/Script/LineWithObject/FixedLineRenderer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FixedLineRenderer : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		GameObject cube_1 = GameObject.Find("Cube1");
+		GameObject cube_2 = GameObject.Find("Cube2");
+		LineRenderer lineY = GameObject.Find ("LineRendererObject").GetComponent<LineRenderer> ();
+		
+		lineY.SetPosition(0, cube_1.transform.position);
+		lineY.SetPosition(1, cube_2.transform.position);
+	}
+}
