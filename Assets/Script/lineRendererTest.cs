cea7f5fce0a441038cf888e72afcf4bd0c28d641
ootsuki <ootsuki@koo-ki.co.jp>
2014-10-20 22:58:48 +0900

    一旦LineRendererによって線を引くシーン（DrawTest）を保存 オブジェクト間で線を引くのを試してみる

---
+++ Assets/Script/lineRendererTest.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class lineRendererTest : MonoBehaviour {
+	
+	public Color c1 = Color.yellow;
+	public Color c2 = Color.red;
+	public int lengthOfLineRenderer = 20;
+	void Start() {
+		LineRenderer lineRenderer = gameObject.GetComponent<LineRenderer>();
+		lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+		lineRenderer.SetColors(c1, c2);
+		lineRenderer.SetWidth(0.2F, 0.2F);
+		lineRenderer.SetVertexCount(lengthOfLineRenderer);
+	}
+	void Update() {
+		LineRenderer lineRenderer = GetComponent<LineRenderer>();
+
+		int index = 0;
+		while (index < lengthOfLineRenderer) {
+		 	Debug.Log ("index =" + index);
+			Vector3 pos = new Vector3(index * 0.5f, Mathf.Sin(index + Time.time), 0);
+			lineRenderer.SetPosition(index, pos);
+			index++;
+		}
+
+		/*int index;
+		for (index = 0; index < 20; index++) {
+			Debug.Log ("index =" + index);
+			Vector3 pos = new Vector3(index * 0.5f, Mathf.Sin(index + Time.time), 0);
+			lineRenderer.SetPosition(index, pos);
+		}*/
+	}
+}
